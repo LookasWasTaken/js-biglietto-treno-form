9cf67e642455dc2c9e90daee62ac78eef38bf7dd
LookasWasTaken~ <piacen.luca@gmail.com>
2023-03-09 17:58:57 +0100

    try fix name show

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,47 +20,72 @@ const generateBtn = document.getElementById("generate");
 const resetBtn = document.getElementById("reset");
 const ticketPreviewEl = document.getElementById("ticket_preview");
 
-generateBtn.addEventListener('click', function() {
+const generateCarriage = Math.floor(Math.random() * 18) + 1;
+console.log(generateCarriage, "Carrozza");
+const generateCPCode = Math.floor(Math.random() * 9999) + 90000;
+console.log(generateCPCode, "CP Code");
+let user = document.getElementById("name_surname").value;
+
+generateBtn.addEventListener("click", function () {
   const km = Number(document.getElementById("km").value);
   const age = document.getElementById("age").value;
   const costKm = 0.21;
   let finalPrice = km * costKm;
 
+  // show name and surname
+
+  const userPreview = document.getElementById("passenger");
+  userPreview.innerHTML = `${user}`;
+
+  // show offer type
+
+  const offerPreview = document.getElementById("offer");
+  offerPreview.innerHTML = `${age}`;
+
+  // show carriage on ticket
+
+  const carriagePreview = document.getElementById("carriage");
+  carriagePreview.innerHTML = `${generateCarriage}`;
+
+  // show code on ticket
+
+  const codePreview = document.getElementById("cpcode");
+  codePreview.innerHTML = `${generateCPCode}`;
+
+  // show cost on ticket
+
+  const ticketPreviewCost = document.getElementById("ticketcost");
+  ticketPreviewCost.innerHTML = `${finalPrice} €`;
+
   switch (age) {
     case "under18":
       finalPrice *= 0.8;
-      console.log(`Price under18: ${(Math.round(finalPrice * 100) / 100).toFixed(2)}€`);
+      console.log(
+        `Price under18: ${(Math.round(finalPrice * 100) / 100).toFixed(2)}€`
+      );
       break;
     case "over65":
       finalPrice *= 0.6;
-      console.log(`Price over 65: ${(Math.round(finalPrice * 100) / 100).toFixed(2)}€`);
+      console.log(
+        `Price over 65: ${(Math.round(finalPrice * 100) / 100).toFixed(2)}€`
+      );
       break;
     default:
-      console.log(`Price over18: ${(Math.round(finalPrice * 100) / 100).toFixed(2)}€`);
+      console.log(
+        `Price over18: ${(Math.round(finalPrice * 100) / 100).toFixed(2)}€`
+      );
   }
 
   // Ta-da, now the ticket shows up
 
   ticketPreviewEl.classList.remove("d-none");
-})
-
+});
 
 // reset
-resetBtn.addEventListener('click', function() {
+
+resetBtn.addEventListener("click", function () {
   ticketPreviewEl.classList.add("d-none");
   document.getElementById("name_surname").value = "";
   km.value = "";
   age.value = "over18";
-})
-
-// 
-
-function generateCarriage() {
-    return Math.floor(Math.random() * 18) + 1;
-  }
-
-// show cost on ticket
-
-const ticketPreviewCost = document.getElementById("ticketcost")
-ticketPreviewCost.innerHTML = `${finalPrice} €`
-
+});
